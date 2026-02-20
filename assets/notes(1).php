1fe1771802fd70989267865905a55f312bc3e9c2
ItsJakee <reddeviljake@msn.com>
2026-02-20 10:35:53 +0000

    Add Trunky-styled sales funnel mockup with local assets

---
+++ assets/notes(1).php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24.9 22.04">
+  <defs>
+    <style>
+      .cls-1 {
+        font-family: MyriadPro-Regular, 'Myriad Pro';
+        font-size: 12px;
+      }
+    </style>
+  </defs>
+  <path d="M18,10.04v-1l-6-6H2c-1.11,0-2,.89-2,2v14c0,1.11.9,2,2,2h6v-1.87l8.39-8.39c.44-.44,1-.68,1.61-.74M11,4.54l5.5,5.5h-5.5v-5.5M19.85,14.23l-.98.98-2.04-2.04.98-.98c.19-.2.52-.2.72,0l1.32,1.32c.2.2.2.53,0,.72M16.13,13.87l2.04,2.04-6.13,6.13h-2.04v-2.04l6.13-6.13Z"/>
+  <text class="cls-1" transform="translate(18.74 10.04)"><tspan x="0" y="0">1</tspan></text>
+</svg>
---
+++ assets/todologo.php
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<svg id="a" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20.41 20.55">
+  <path d="M16,0H2C.9,0,0,.9,0,2v14c0,1.1.9,2,2,2h14c1.1,0,2-.9,2-2V2c0-1.1-.9-2-2-2M16,2v14H2V2h14M7,14l-4-4,1.41-1.42,2.59,2.59,6.59-6.59,1.41,1.42"/>
+  </svg>
